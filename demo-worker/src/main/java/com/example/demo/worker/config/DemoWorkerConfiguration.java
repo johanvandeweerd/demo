2d7fa4d89986413a4c5e8742354d0544c81e98c4
Johan Vandeweerd <johan.v@sparkcentral.com>
2019-03-22 15:30:25 +0100

    Use KafkaConsumer instead of Spring KafkaListener

--- demo-worker/src/main/java/com/example/demo/worker/config/DemoWorkerConfiguration.java
+++ demo-worker/src/main/java/com/example/demo/worker/config/DemoWorkerConfiguration.java
@@ -1,7 +1,37 @@
 package com.example.demo.worker.config;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 
 @Configuration
 public class DemoWorkerConfiguration {
+
+    @Bean
+    public Consumer<Long, String> createConsumer(Properties consumerProperties) {
+        Consumer<Long, String> consumer = new KafkaConsumer<>(consumerProperties);
+        consumer.subscribe(Collections.singletonList("hello-world"));
+        return consumer;
+    }
+
+    @Bean
+    public Properties consumerProperties() {
+        Properties props = new Properties();
+        props.put(BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(GROUP_ID_CONFIG, "hello-world-group");
+        props.put(KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(MAX_POLL_RECORDS_CONFIG, 1);
+        props.put(ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
+    }
 }
--- demo-worker/src/main/java/com/example/demo/worker/service/WorkerListener.java
+++ demo-worker/src/main/java/com/example/demo/worker/service/WorkerListener.java
@@ -1,19 +1,38 @@
 package com.example.demo.worker.service;
 
-import com.example.demo.api.HelloWorldMessage;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 
 @Service
 public class WorkerListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkerListener.class);
 
-    @KafkaListener(topics = "hello-world")
-    public void listen(ConsumerRecord<String, HelloWorldMessage> consumerRecord) {
+    private final Consumer<Long, String> consumer;
+
+    public WorkerListener(Consumer<Long, String> consumer) {
+        this.consumer = consumer;
+    }
+
+    @PostConstruct
+    public void consume() {
+        while (true) {
+            LOGGER.debug("Polling kafka");
+            ConsumerRecords<Long, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            LOGGER.debug("Processing {} consumerRecords", consumerRecords.count());
+            consumerRecords.forEach(this::process);
+            consumer.commitAsync();
+        }
+    }
+
+    private void process(ConsumerRecord<Long, String> consumerRecord) {
         LOGGER.info("Received message with headers {} and payload {}", consumerRecord.headers(), consumerRecord.value());
         sleep();
         LOGGER.info("Processed message with {} and payload {}", consumerRecord.headers(), consumerRecord.value());
